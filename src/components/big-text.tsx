d870c936ed1c4e711bae953d1f4abf91b0072de3
pavel.genuine <pavel.playstore@gmail.com>
2026-04-30 20:13:10 +0600

    footer about

--- src/components/big-text.tsx
+++ src/components/big-text.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import React from "react";
 
-// type 
+// type
 type IProps = {
   cls?: string;
 };
 
-export default function BigText({cls=""}: IProps) {
+export default function BigText({ cls = "" }: IProps) {
   return (
     <div className={`sv-big-text-area pb-80 ${cls}`}>
       <div className="container container-1530">
@@ -16,7 +16,7 @@ export default function BigText({cls=""}: IProps) {
         </div>
         <div className="sv-big-text-box">
           <h4 className="sv-big-text tp-char-animation">
-            <Link href="/contact">Get in Touch</Link>
+            <Link href="/lets-connect">Get in Touch</Link>
           </h4>
         </div>
       </div>
